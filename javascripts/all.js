07a9a2841057a5379056b399cb5ec0b5b830f09e
James Hart <hjhart@gmail.com>
2013-05-16 23:28:57 -0700

    Automated commit at 2013-05-17 06:28:57 UTC by middleman-deploy 3.0.14

--- javascripts/all.js
+++ javascripts/all.js
@@ -13125,3 +13125,12 @@ if (typeof jQuery === "undefined" &&
 
 
 
+
+(function() {
+  var conference_date = new Date(2013, 8, 20);
+  var today = new Date();
+  var one_day_in_milliseconds = (1000 * 60 * 24 * 60);
+  var days_until_conference = Math.ceil((conference_date - today) / one_day_in_milliseconds);
+  $('#days_until_conference #number').html(days_until_conference);
+})()
+;
